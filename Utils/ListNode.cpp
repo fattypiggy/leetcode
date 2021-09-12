ef535579f94c91df6ee6bb0c7e1e0c5062b8a1ee
William Jing <monkeykingisback@gmail.com>
2021-09-12 20:09:30 +0800

    Create Utils/ListNode

---
+++ Utils/ListNode.cpp
@@ -0,0 +1,106 @@
+#include <ListNode.h>
+#include <cstdio>
+#include <cstdlib>
+
+ListNode* CreateListNode(int value)
+{
+    ListNode* pNode = new ListNode(value);
+    return pNode;
+}
+
+void ConnectListNodes(ListNode* pCurrent, ListNode* pNext)
+{
+    if(pCurrent == nullptr)
+    {
+        printf("Error to connect two nodes.\n");
+        exit(1);
+    }
+
+    pCurrent->next = pNext;
+}
+
+void PrintListNode(ListNode* pNode)
+{ 
+    if(pNode == nullptr)
+    {
+        printf("The node is nullptr\n");
+    }
+    else
+    {
+        printf("The key in node is %d.\n", pNode->val);
+    }
+}
+
+void PrintList(ListNode* pHead)
+{
+    printf("PrintList starts.\n");
+    
+    ListNode* pNode = pHead;
+    while(pNode != nullptr)
+    {
+        printf("%d\t", pNode->val);
+        pNode = pNode->next;
+    }
+
+    printf("\nPrintList ends.\n");
+}
+
+void DestroyList(ListNode* pHead)
+{
+    ListNode* pNode = pHead;
+    while(pNode != nullptr)
+    {
+        pHead = pHead->next;
+        delete pNode;
+        pNode = pHead;
+    }
+}
+
+void AddToTail(ListNode** pHead, int value)
+{
+    ListNode* pNew = new ListNode(value);
+
+    if(*pHead == nullptr)
+    {
+        *pHead = pNew;
+    }
+    else
+    {
+        ListNode* pNode = *pHead;
+        while(pNode->next != nullptr)
+            pNode = pNode->next;
+
+        pNode->next = pNew;
+    }
+}
+
+void RemoveNode(ListNode** pHead, int value)
+{
+    if(pHead == nullptr || *pHead == nullptr)
+        return;
+
+    ListNode* pToBeDeleted = nullptr;
+    if((*pHead)->val == value)
+    {
+        pToBeDeleted = *pHead;
+        *pHead = (*pHead)->next;
+    }
+    else
+    {
+        ListNode* pNode = *pHead;
+        while(pNode->next != nullptr && pNode->next->val != value)
+            pNode = pNode->next;
+
+        if(pNode->next != nullptr && pNode->next->val == value)
+        {
+            pToBeDeleted = pNode->next;
+            pNode->next = pNode->next->next;
+        }
+    }
+
+    if(pToBeDeleted != nullptr)
+    {
+        delete pToBeDeleted;
+        pToBeDeleted = nullptr;
+    }
+}
---
+++ Utils/ListNode.h
@@ -0,0 +1,13 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int val): val(val), next(nullptr) {}
+};
+
+ListNode* CreateListNode(int value);
+void ConnectListNodes(ListNode* pCurrent, ListNode* pNext);
+void PrintListNode(ListNode* pNode);
+void PrintList(ListNode* pHead);
+void DestroyList(ListNode* pHead);
+void AddToTail(ListNode** pHead, int value);
+void RemoveNode(ListNode** pHead, int value);
